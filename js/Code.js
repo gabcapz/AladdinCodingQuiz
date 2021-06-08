7bb1ece821ee7976cd96c393ecccc104adaef593
gabcapz <75875376+gabcapz@users.noreply.github.com>
2021-06-08 13:37:44 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,29 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "BOOLEAN";
+const accessCode3 = "ARRAY";
+
+function clues() {
+    
+    fill("white");
+    textSize(15);
+    textStyle(ITALIC);
+    text("R E V B A I L A", 100,50);
+    fill("lightblue");
+    text("Hint: It's always changing, not constant !!", 100,70);
+// add code for displaying rest of the names and the hints.
+fill("white");
+textSize(15);
+textStyle(ITALIC);
+text("O B O E L N A", 650,150);
+fill("lightblue");
+text("Hint: It represents one of two values: true or false !!", 650,170);
+
+fill("white");
+textSize(15);
+textStyle(ITALIC);
+text("R A R A Y", 100,330);
+fill("lightblue");
+text("Hint: It is used to store multiple values in a single variable !!", 100,350);
+    
+
+}
